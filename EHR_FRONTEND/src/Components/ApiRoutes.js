ebf41888af84bbb9279d08fc340a1014a09aec46
JuanFlorez1326 <juanks339@gmail.com>
2022-07-01 12:07:47 -0500

    Proyecto EHR en LocalHost

---
+++ EHR_FRONTEND/src/Components/ApiRoutes.js
@@ -0,0 +1,6 @@
+export const urlUsers = "https://localhost:5001/api/Users";
+export const urLDepartments = "https://localhost:5001/api/Department";
+export const urlMunicipality = "https://localhost:5001/api/Municipality/0?iddepartamento=";
+export const urlLogin = "https://localhost:5001/api/Login"
+export const urlSearch = "https://localhost:5001/api/home"
+export const urlAdversitement = 'https://localhost:5001/api/Advertisement/'
